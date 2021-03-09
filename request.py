902510ed3d27131c6dc1fc4dc414e8a74cbaeb16
Petro <petro@Petro-MacBook-Pro.local>
2021-03-09 21:52:45 +0200

    Added python script

---
+++ request.py
@@ -0,0 +1,38 @@
+import requests
+import time
+import datetime
+import calendar
+import json
+import os
+
+api_key = ""
+page_id = ""
+metric_id = ""
+
+get_header = {'Authorization': 'Bearer fanteam undefined'}
+auth_header = {'Authorization': api_key}
+service_url = 'https://fanteam-game.api.scoutgg-stg.net/users/@me'
+statuspage_url = f"https://api.statuspage.io/v1/pages/{page_id}/metrics/data"
+
+while True:
+    get_request = requests.get(service_url, headers=get_header)
+    latency = get_request.headers['X-Response-Time']
+
+    timestamp = calendar.timegm(time.gmtime())
+
+    data = {
+        "data": {
+        metric_id: [
+            {
+                "timestamp": timestamp,
+                "value": latency
+            }
+        ]
+        }
+    }
+    print(data)
+
+    post_request = requests.post(statuspage_url, headers = auth_header, data = json.dumps(data) )
+    print(post_request.status_code)
+
+    time.sleep(30)
